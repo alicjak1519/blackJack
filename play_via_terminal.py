e32b881c763eff99bef134ca4fec4fee94990b8a
Alicja <akrzeminska1519@gmail.com>
2020-04-09 18:09:57 +0200

    Save scores and print scores function

--- play_via_terminal.py
+++ play_via_terminal.py
@@ -1,7 +1,7 @@
 from classes import Player
 from classes import BlackJackGame
 
-my_player = Player("John")
+my_player = Player("John2")
 pack_number = 3
 
 my_game = BlackJackGame(my_player, pack_number)
