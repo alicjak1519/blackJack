ea7824091875b3376c9624a2e3753fb529f58e2a
Alicja <akrzeminska1519@gmail.com>
2020-06-16 09:14:08 +0200

    Fixed a couple of bugs

--- play_via_terminal.py
+++ play_via_terminal.py
@@ -1,13 +1,15 @@
-from classes import Player
-from classes import BlackJackGame
+from classes import Player, BlackJackGame, print_scores
 
-my_player = Player("John")
-pack_number = 3
+my_player = Player(input("What is your name?\n"))
+pack_number = int(input("How many packs you want to play?\n"))
+
+if input("Do you want to see scoreboard? [y/n] \n") == "y":
+    print_scores()
 
 my_game = BlackJackGame(my_player, pack_number)
 
 try:
-    my_game.save_score()
+    my_game.play_a_game()
 except Exception as exception:
     print("Try again, something went wrong!")
     print(exception)
